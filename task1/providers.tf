da3e6354d003a09e1d6378d5d9a3b3afcec397fd
vitaliisirantpp2022-lang <vitalii.sirant.pp.2022@lpnu.ua>
2026-04-04 14:35:37 +0300

    Update providers.tf

--- task1/providers.tf
+++ task1/providers.tf
@@ -9,8 +9,8 @@ terraform {
   # Вимога: tfstate-файл розміщений у хмарі
   backend "s3" {
     endpoint                    = "fra1.digitaloceanspaces.com"
-    region                      = "us-east-1" # Для S3 backend у DO завжди залишаємо us-east-1 для сумісності API
-    bucket                      = "fedorychko-tfstate" # Бакет, який ви створили на Етапі 1
+    region                      = "us-east-1" 
+    bucket                      = "sirant-tfstate-exam" 
     key                         = "infrastructure/terraform.tfstate"
     skip_credentials_validation = true
     skip_requesting_account_id  = true
@@ -21,5 +21,4 @@ terraform {
 }
 
 provider "digitalocean" {
-  # Токен буде підтягуватися автоматично через змінні оточення у GitHub Actions
 }
