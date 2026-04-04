94b7f053fd5eb6e10c0b481bf034eb59aaa27816
vitaliisirantpp2022-lang <vitalii.sirant.pp.2022@lpnu.ua>
2026-04-04 15:05:41 +0300

    Add TLS private key and update SSH key resource
    Added a new TLS private key resource and updated SSH key references in the DigitalOcean configuration.

--- task1/main.tf
+++ task1/main.tf
@@ -1,7 +1,13 @@
-data "digitalocean_ssh_key" "existing" {
-  name = "sirant-control-node-key" 
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
+resource "digitalocean_ssh_key" "main_key" {
+  name       = "sirant-ssh-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
 
 resource "digitalocean_vpc" "k8s_vpc" {
   name     = "sirant-vpc"
@@ -9,39 +15,30 @@ resource "digitalocean_vpc" "k8s_vpc" {
   ip_range = "10.10.10.0/24"
 }
 
-
 resource "digitalocean_droplet" "node" {
   name     = "sirant-node"
   region   = "fra1"
-  size     = "s-2vcpu-4gb" # 2 vCPU та 4GB RAM - ідеально для Minikube
+  size     = "s-2vcpu-4gb"
   image    = "ubuntu-24-04-x64"
   vpc_uuid = digitalocean_vpc.k8s_vpc.id
-  ssh_keys = [data.digitalocean_ssh_key.existing.id]
+  ssh_keys = [digitalocean_ssh_key.main_key.id]
 }
-
 
 resource "digitalocean_firewall" "k8s_firewall" {
   name = "sirant-firewall"
-
   droplet_ids = [digitalocean_droplet.node.id]
 
   dynamic "inbound_rule" {
     for_each = [22, 80, 443, 8000, 8001, 8002, 8003]
     content {
       protocol         = "tcp"
-      port_range       = tostring(inbound_rule.value)
+      port_range       = inbound_rule.value
       source_addresses = ["0.0.0.0/0", "::/0"]
     }
   }
 
   outbound_rule {
     protocol              = "tcp"
-    port_range            = "1-65535"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-  
-  outbound_rule {
-    protocol              = "udp"
     port_range            = "1-65535"
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
