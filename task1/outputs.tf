cd22782119ac0202e30bfe8d4e8c5cf0a1434402
vitaliisirantpp2022-lang <vitalii.sirant.pp.2022@lpnu.ua>
2026-04-04 15:07:04 +0300

    Add private_key output to outputs.tf
    Added output for private SSH key.

--- task1/outputs.tf
+++ task1/outputs.tf
@@ -1,4 +1,8 @@
 output "vm_public_ip" {
-  value       = digitalocean_droplet.node.ipv4_address
-  description = "10.114.0.2"
+  value = digitalocean_droplet.node.ipv4_address
+}
+
+output "private_key" {
+  value     = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
 }
